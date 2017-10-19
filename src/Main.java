fc4e537c5799545963aefc9a9ea9436122117897
Drew Morton <drew.morton94@gmail.com>
2017-10-19 12:36:56 -0400

    Add sysout test for CharacterBuilder

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 // @author Nick McCarty & Drew Morton
 // Txt RPG created by Drewbie Dew and Nickie Poo
 
-// This will be the Main method that will be the method that runs the game.
+// This will be the Main class that will configure and run the game.
 
 import java.util.*;
 import java.awt.BorderLayout;
@@ -17,9 +17,24 @@ public class Main {
 	
 	private static File file;
 	private static ArrayList<String> rooms = new ArrayList<String>();
-	public Character C;
+	public Character human;
+	
+
+// This will be the Main method that will be the method that runs the game.
 	
 	public static void main(String[] args){
+		
+		//Create character Human
+		human = new Character.CharacterBuilder("Drew", "Swing", 100, 10, "Mighty Swing", "Sword");
+		
+		//TO DO: Test the below. If they print then they work as expected.
+		System.out.println(human.getName());
+		System.out.println(human.getAttack());
+		System.out.println(human.getHealth());
+		System.out.println(human.getPower());
+		System.out.println(human.getPowerName());
+		System.out.println(human.getWeapon());
+		
 		setFile("World.txt");
 		scanTextFile(file);
 	    Room Room = new Room(getRooms().get(0), rooms.get(1), rooms.get(2), rooms.get(3), rooms.get(4));
@@ -72,7 +87,6 @@ public class Main {
 
 	public static void setFile(String fileName) {
 		Main.file = new File(fileName);
-		C = new Character.CharacterBuilder("Drew", "Swing", 100, 10, "Mighty Swing", "Sword")
 	}
 
 }
